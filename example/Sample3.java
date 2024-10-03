c71b9675eec1b9506acc267984bd80c49ed3855c
111360126 <t111360126@ntut.org.tw>
2024-10-03 18:42:58 +0800

    first commit

---
+++ example/Sample3.java
@@ -0,0 +1,14 @@
+package example;
+
+public class Sample3 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("A");
+		System.out.println("歡迎使用Java吧!");
+		System.out.println("123");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ example/Sample7.java
@@ -0,0 +1,14 @@
+package example;
+
+public class Sample7 {
+
+	public static void main(String[] args) 
+	{
+		int num;
+		num = 3;
+		System.out.println("變數num的值是"+num);
+		// TODO Auto-generated method stub
+
+	}
+
+}
